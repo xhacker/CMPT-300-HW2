45eb02ea24390e248f21a7ebf028bd555f5f6135
Xhacker Liu <liu.dongyuan@gmail.com>
2014-03-10 14:33:43 -0700

    Run and pause/resume button.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    paused = false;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_runButton_clicked()
+{
+    total_operators = ui->operatorsSpinBox->value();
+    total_tools = ui->toolsSpinBox->value();
+
+    ui->pauseButton->setEnabled(true);
+
+    cout << "Running: " << total_operators << " operators, " << total_tools << " tools." << endl;
+}
+
+void MainWindow::on_pauseButton_clicked()
+{
+    paused = !paused;
+    if (paused) {
+        ui->pauseButton->setText("Resume");
+    }
+    else {
+        ui->pauseButton->setText("Pause");
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_runButton_clicked();
+
+    void on_pauseButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    int total_operators;
+    int total_tools;
+    bool paused;
 };
 
 #endif // MAINWINDOW_H
